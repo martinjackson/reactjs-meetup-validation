02874a9d9c274c1f91eb08ea7bb6ea8d09731ac3
Martin Jackson <martin.a.jackson@gmail.com>
2016-04-18 14:47:39 -0500

    found how to get webpack-dev-server to proxy to other process (node server) for server side development as well

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,10 @@ module.exports = {
     },
   devServer: {
     inline: true,
-    port: 8080
+    port: 8080,
+    proxy: {
+    '/projects/*': 'http://localhost:80/' // <- backend
+      }
   },
   module: {
     loaders: [
